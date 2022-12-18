dcfb36b971ba32b8fc5a11a798ef69d67e39bed9
juancansi <juancansi@gmail.comgit config --global user.name Your>
2022-12-18 01:01:18 -0300

    delete vite

---
+++ src/providers/NumberProvider.js
@@ -0,0 +1,24 @@
+import { useContext, createContext, useState } from 'react';
+
+const numberContext = createContext();
+const numberToggleContext = createContext();
+
+export const CurrentNumberProvider = ({ children }) => {
+  const [currentNumber, setCurrentNumber] = useState();
+
+  return (
+    <numberContext.Provider value={currentNumber}>
+        <numberToggleContext.Provider value={setCurrentNumber}>
+            {children}
+        </numberToggleContext.Provider>
+    </numberContext.Provider>
+  );
+};
+
+export const useCurrentNumberContext = () => {
+  return useContext(numberContext);
+};
+
+export const useCurrentNumberToggleContext = () => {
+  return useContext(numberToggleContext);
+};
