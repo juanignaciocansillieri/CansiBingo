75bb756f3fb77e2a994139c7017b94b83c988cb2
juancansi <juancansi@gmail.comgit config --global user.name Your>
2023-02-11 04:00:48 -0300

    Providers in app

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CurrentNumberProvider } from './providers/NumberProvider';
+import { GameStartedProvider } from './providers/GameStartedProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CurrentNumberProvider>
+      <GameStartedProvider>
+        <App />
+      </GameStartedProvider>
+    </CurrentNumberProvider>
   </React.StrictMode>,
 );
 reportWebVitals();
