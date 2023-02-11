2b82b22134b8a7a55e9b2ef73948e52ab6c86f1a
juancansi <juancansi@gmail.comgit config --global user.name Your>
2023-02-11 03:58:31 -0300

    Start game: - set the bowler and shuffle - set current and last numbers - rotate balls style

--- src/components/LeftPanel/LeftPanel.js
+++ src/components/LeftPanel/LeftPanel.js
@@ -1,22 +1,81 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './leftPanel.scss';
-import currentBall from '../../assets/img/currentBall.png';
+import imgCurrentBall from '../../assets/img/currentBall.png';
+import { useCurrentNumberContext, useCurrentNumberToggleContext } from '../../providers/NumberProvider';
 import Ball from '../Ball/Ball';
+import { useGameStartedContext, useGameStartedToggleContext } from '../../providers/GameStartedProvider';
 
 const LeftPanel = () => {
+  const [penultimateNumber, setPenultimateNumber] = useState();
+  const [antepenultimateNumber, setAntepenultimate] = useState();
+  const [angle, setAngle] = useState(0);
+  const [counter, setCounter] = useState(1);
+  const [bowler, setBowler] = useState([]);
+  const rotate = `rotate(${angle}deg)`;
+  const currentNumber = useCurrentNumberContext();
+  const setCurrentNumber = useCurrentNumberToggleContext();
+  const isGameStarted = useGameStartedContext();
+  const setIsGameStarted = useGameStartedToggleContext();
+
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i -= 1) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  };
+
+  const generateRandomNumbers = () => {
+    const numbers = Array.from({ length: (90 - 1) / 1 + 1 }, (_, i) => 1 + (i * 1));
+    shuffleArray(numbers);
+    setBowler(numbers);
+    setAngle(0);
+    setCounter(1);
+  };
+
+  const resetGame = () => {
+    generateRandomNumbers();
+    setCurrentNumber('');
+    setIsGameStarted(false);
+    setAntepenultimate('');
+    setPenultimateNumber('');
+    setAngle(angle);
+  };
+  const startBingo = () => {
+    console.log(bowler);
+    console.log(currentNumber);
+    setIsGameStarted(true);
+    setAntepenultimate(penultimateNumber);
+    setPenultimateNumber(currentNumber);
+    const number = bowler.shift();
+    setCurrentNumber(number);
+    setAngle(1080 + angle);
+    setCounter(counter + 1);
+
+    if (counter === 91) {
+      resetGame();
+    }
+  };
+  useEffect(() => {
+    generateRandomNumbers();
+  }, []);
+  useEffect(() => {
+    if (!isGameStarted) resetGame();
+  }, [isGameStarted]);
+
   return (
     <div className='panelLeftContainer'>
       <div className='currentBall span-3-grid-column' >
-        <img src={currentBall} alt="Current Ball" />
-        <h1>85</h1>
+        <img src={imgCurrentBall} width='85%' alt="Current Ball" style={{ transform: rotate, transition: 'transform 1s' }} />
+        <h1 style={{ transform: rotate, transition: 'transform 1s' }}>{currentNumber}</h1>
       </div>
       <div className='previousBalls'>
-        <Ball />
-        <Ball />
-        <Ball />
+        <Ball number={currentNumber} />
+        <Ball number={penultimateNumber} />
+        <Ball number={antepenultimateNumber} />
       </div>
       <div className='button'>
-        <button className='startButton'>Empezar a jugar</button>
+        <button className='startButton' onClick={startBingo}>{`${isGameStarted ? 'Siguiente Número' : 'Iniciar juego'}`}</button>
+        <button className='resetButton' onClick={() => setIsGameStarted(false)}>↪</button>
       </div>
     </div>
   );
