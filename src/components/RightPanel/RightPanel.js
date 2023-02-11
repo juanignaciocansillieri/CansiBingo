e9865102fcf81a00f62a00b4f39ed1977c0bd72e
juancansi <juancansi@gmail.comgit config --global user.name Your>
2023-02-11 04:02:26 -0300

    Fill and empty pass numbers

--- src/components/RightPanel/RightPanel.js
+++ src/components/RightPanel/RightPanel.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './rightPanel.scss';
+import { useCurrentNumberContext } from '../../providers/NumberProvider';
+import { useGameStartedContext } from '../../providers/GameStartedProvider';
 
 const RightPanel = () => {
+  const currentNumber = useCurrentNumberContext();
+  const isGameStarted = useGameStartedContext();
+  const numberRef = useRef();
   const numbers = Array.from({ length: (90 - 1) / 1 + 1 }, (_, i) => 1 + (i * 1));
+
+  const fillNumbers = () => {
+    const span = numberRef.current.childNodes[currentNumber - 1].firstChild;
+    span.className = 'activeNum';
+  };
+  const emptyNumbers = () => {
+    const allSpans = numberRef.current.childNodes;
+    allSpans.forEach((currentSpan) => {
+      currentSpan.firstChild.className = '';
+    });
+  };
+  useEffect(() => {
+    if (isGameStarted) fillNumbers();
+    else emptyNumbers();
+  }, [currentNumber]);
+  useEffect(() => {
+    if (!isGameStarted) emptyNumbers();
+  }, [isGameStarted]);
 
   return (
     <div className='panelRightContainer'>
-      <div className='numContainer'>
+      <div className='numContainer' ref={numberRef}>
         {numbers.map((num, index) => <div className='num' style={{ textAlign: 'center' }} key={index}>
           <span id={num} >{num}</span>
         </div>)}
